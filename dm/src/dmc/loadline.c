ceaccc659b7b078afd96f62f6eba4b5be8de78de
Walter Bright <walter@walterbright.com>
2013-09-21 15:32:47 -0700

    remove unused 16 bit implementation

--- dm/src/dmc/loadline.c
+++ dm/src/dmc/loadline.c
@@ -10,11 +10,6 @@
  * For any other uses, please contact Digital Mars.
  */
 
-#if __INTSIZE == 2 && !(__SMALL__ || __COMPACT__)
-// Put us in the same segment as token.c so we can use near calls
-#pragma cseg TOKEN_TEXT
-#endif
-
 #include        <stdio.h>
 #include        <string.h>
 #include        <stdlib.h>
@@ -27,153 +22,6 @@
 extern char switch_E;
 
 #if _WIN32
-
-#if __INTSIZE == 2
-
-/****************************************************
- * Read line from stream until we encounter an error.
- * Requires:
- *      S or M model, and BIGBUF
- * Returns:
- *      c
- */
-
-int loadline (FILE *fp,unsigned char **pp,unsigned char *ptop)
-{
-    _asm
-    {
-        push    DS
-
-        lds     BX,pp                   ;BX = &p
-        mov     DX,01A0Ah               ;DH = ^Z, DL = '\n'
-        les     DI,[BX]                 ;ES:DI = p
-        mov     CX,word ptr ptop        ;CX = ptop
-        sub     CX,DI                   ;CX = # of chars we can still put
-        inc     CX                      ; in buffer
-        lds     BX,fp                   ;BX = fp
-        mov     AX,_cnt[BX]
-        lds     SI,_ptr[BX]
-        mov     BX,AX
-        jmp     short G1
-
-        even
-L3:     stosb                           ;store char from I/O buffer into *p
-G1:     dec     BX
-        js      G2                      ;if out of chars in buffer
-G4:     lodsb                           ;get char from buffer (DS:SI)
-        cmp     AL,DH
-        je      G3                      ;^Z marks end of file
-        cmp     AL,DL                   ;is AL a newline?
-        loopne  L3
-L4:     xor     AH,AH
-
-L1:     mov     CX,BX
-        lds     BX,fp
-G5:     mov     _cnt[BX],CX
-        mov     word ptr _ptr[BX],SI    ;updated I/O buffer pointer
-        lds     BX,pp
-        mov     [BX],DI                 ;updated p
-        pop     DS
-    }
-    return _AX;
-
-    _asm
-    {
-
-G3:     lds     BX,fp
-        xor     CX,CX                           ;0 chars left in buffer
-        mov     AX,EOF
-        or      byte ptr _flag[BX],_IOEOF       ;set EOF flag bit
-        jmp     G5
-
-G2:     pop     DS
-        push    DS                      ;restore DS to original value
-        push    CX
-        push    ES                      ;save
-    }
-    _fillbuf(fp);                       // fill buffer
-    _asm
-    {
-        mov     DX,01A0Ah               ;DH = ^Z, DL = '\n'
-        lds     BX,fp
-        mov     CX,_cnt[BX]
-        lds     SI,_ptr[BX]
-        mov     BX,CX
-        pop     ES
-        pop     CX
-        test    AX,AX                   ;EOF?
-        jnz     L1                      ;yes
-        dec     BX
-        jmp     G4
-    }
-}
-
-/*************************************************
- * Read in identifier and store it in tok_ident[].
- * Input:
- *      xc =    first char of identifier
- * Output:
- *      xc =    first char past identifier
- *      tok_ident[]     holds the identifier
- */
-
-__declspec(naked) void __near inident()
-{
-    _asm
-    {
-        push    DI
-        mov     DI,offset tok_ident             ;p = tok_ident
-        mov     AX,xc                           ;AH is set to 0
-        mov     [DI],AL
-        inc     DI
-
-        shl     AX,10
-        mov     idhash,AX                       ;idhash = xc << 10;
-
-        even
-L26:
-        call    egchar
-        mov     BX,AX
-        test    byte ptr _chartype[BX + 1],3    ;if !isidchar(xc)
-        jz      L65
-L3A:
-        cmp     DI,offset tok_ident + IDMAX
-        jae     L4E
-        mov     [DI],AL
-        inc     DI                      ;*p++ = xc
-
-        call    egchar
-        mov     BX,AX
-        test    byte ptr _chartype[BX + 1],3    ;if !ididchar(xc)
-        jz      L65
-        cmp     DI,offset tok_ident + IDMAX
-        jae     L4E
-        mov     [DI],AL
-        inc     DI                      ;*p++ = xc
-        jmp     L26
-    }
-
-L4E:
-    lexerr(EM_ident2big);               // identifier is too long
-    goto L26;
-
-    _asm
-    {
-L65:    mov     [DI],AH                 ;AH is 0 (terminate string)
-
-;  idhash += ((p - tok_ident) << 6) + (*(p - 1) & 0x3F);
-        mov     AL,-1[DI]
-        sub     DI,offset tok_ident
-        and     AL,03Fh
-        shl     DI,6
-        add     AX,DI
-        add     idhash,AX
-        pop     DI
-        ret
-    }
-}
-
-#else
 
 /****************************************************
  * Read line from stream until we encounter an error.
@@ -380,5 +228,4 @@ Lspecial:
     }
 }
 
-#endif
 #endif
