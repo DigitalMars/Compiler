27a5f59fc1248089b8b9377cd9117ef675763c96
Walter Bright <walter@walterbright.com>
2017-07-19 14:30:00 -0700

    convert adl.c to adl.d

--- dm/src/dmc/adl.d
+++ dm/src/dmc/adl.d
@@ -6,32 +6,43 @@
  * Authors:     $(LINK2 http://www.digitalmars.com, Walter Bright)
  * License:     Distributed under the Boost Software License, Version 1.0.
  *              http://www.boost.org/LICENSE_1_0.txt
- * Source:      https://github.com/DigitalMars/Compiler/blob/master/dm/src/dmc/adl.c
+ * Source:      https://github.com/DigitalMars/Compiler/blob/master/dm/src/dmc/adl.d
  */
 
 // CPP98 3.4.2 Argument-dependent name lookup
 
+module adl;
 
-#if !SPP
+version (SPP)
+{
+}
+else
+{
 
-#include        <stdio.h>
-#include        <ctype.h>
-#include        <string.h>
-#include        <stdlib.h>
-#include        "cc.h"
-#include        "parser.h"
-#include        "token.h"
-#include        "global.h"
-#include        "oper.h"
-#include        "el.h"
-#include        "type.h"
-#include        "cpp.h"
-#include        "cgcv.h"
+import core.stdc.ctype;
+import core.stdc.stdio;
+import core.stdc.stdlib;
+import core.stdc.string;
 
-static char __file__[] = __FILE__;      /* for tassert.h                */
-#include        "tassert.h"
+import ddmd.backend.cc;
+import ddmd.backend.cdef;
+import ddmd.backend.cgcv;
+import ddmd.backend.el;
+import ddmd.backend.global;
+import ddmd.backend.oper;
+import ddmd.backend.ty;
+import ddmd.backend.type;
 
-#define LOG     0
+import cpph;
+import dtoken;
+import parser;
+import scopeh;
+
+import tk.dlist;
+
+extern (C++):
+
+//debug = LOG;
 
 void adl_scanType(list_t *pl, type *t);
 
@@ -39,11 +50,9 @@ void adl_scanType(list_t *pl, type *t);
  * Add namespace or class to list *pl.
  */
 
-void adl_addSymbol(list_t *pl, symbol *s)
+void adl_addSymbol(list_t *pl, Symbol *s)
 {
-#if LOG
-    printf("\taddSymbol('%s')\n", s ? s->Sident : "");
-#endif
+    debug (LOG) printf("\taddSymbol('%s')\n", s ? s.Sident : "");
     if (/*s &&*/ !list_inlist(*pl, s))
     {
         list_prepend(pl, s);
@@ -58,12 +67,12 @@ void adl_addClassNamespace(list_t *pl, Classsym *s)
 {
     if (!list_inlist(*pl, s))
     {
-        adl_addSymbol(pl, s->Sscope);
+        adl_addSymbol(pl, s.Sscope);
 
         // Add direct and indirect base classes, and the
         // namespaces they are defined in
-        for (baseclass_t *b = s->Sstruct->Sbase; b; b = b->BCnext)
-            adl_addClassNamespace(pl, b->BCbase);
+        for (baseclass_t *b = s.Sstruct.Sbase; b; b = b.BCnext)
+            adl_addClassNamespace(pl, b.BCbase);
     }
 }
 
@@ -81,17 +90,17 @@ void adl_addClass(list_t *pl, Classsym *s)
         adl_addSymbol(pl, s);
 
         // If instance of a template
-        if (s->Sstruct->Stempsym)
+        if (s.Sstruct.Stempsym)
         {
-            adl_addSymbol(pl, s->Sstruct->Stempsym->Sscope);
+            adl_addSymbol(pl, s.Sstruct.Stempsym.Sscope);
 
             // BUG: Should we use Spr_arglist instead?
             // BUG: What about types in default template arguments?
-            for (param_t *p = s->Sstruct->Sarglist; p; p = p->Pnext)
+            for (param_t *p = s.Sstruct.Sarglist; p; p = p.Pnext)
             {
                 // BUG: Review CPP98 rules for template template arguments
-                if (p->Ptype)
-                    adl_scanType(pl, p->Ptype);
+                if (p.Ptype)
+                    adl_scanType(pl, p.Ptype);
             }
         }
     }
@@ -104,11 +113,10 @@ void adl_addClass(list_t *pl, Classsym *s)
 
 void adl_scanType(list_t *pl, type *t)
 {
-    tym_t ty;
-    symbol *s;
+    Symbol *s;
 
-    type_debug(t);
-    ty = tybasic(t->Tty);
+    //type_debug(t);
+    tym_t ty = tybasic(t.Tty);
     switch (ty)
     {
         case TYvoid:
@@ -117,43 +125,43 @@ void adl_scanType(list_t *pl, type *t)
 
         case TYarray:
         case TYref:
-            adl_scanType(pl, t->Tnext);
+            adl_scanType(pl, t.Tnext);
             break;
 
         case TYstruct:
-            s = t->Ttag;
-            if (s->Sstruct->Sflags & STRunion)  // if a union
-                adl_addSymbol(pl, s->Sscope);
+            s = t.Ttag;
+            if (s.Sstruct.Sflags & STRunion)  // if a union
+                adl_addSymbol(pl, s.Sscope);
             else
-                adl_addClass(pl, (Classsym *)s);
+                adl_addClass(pl, cast(Classsym *)s);
             break;
 
         case TYenum:
-            adl_addSymbol(pl, t->Ttag->Sscope);
+            adl_addSymbol(pl, t.Ttag.Sscope);
             break;
 
         case TYmemptr:          // pointer to data or function member
-            adl_addClass(pl, t->Ttag);
-            adl_scanType(pl, t->Tnext);
+            adl_addClass(pl, t.Ttag);
+            adl_scanType(pl, t.Tnext);
             break;
 
         default:
             if (typtr(ty))
             {
-                adl_scanType(pl, t->Tnext);
+                adl_scanType(pl, t.Tnext);
             }
             else if (tyfunc(ty))
             {
                 // Do parameters
-                for (param_t *p = t->Tparamtypes; p; p = p->Pnext)
+                for (param_t *p = t.Tparamtypes; p; p = p.Pnext)
                 {
-                    if (p->Ptype)
-                        adl_scanType(pl, p->Ptype);
+                    if (p.Ptype)
+                        adl_scanType(pl, p.Ptype);
                 }
-                adl_scanType(pl, t->Tnext);     // and return type
+                adl_scanType(pl, t.Tnext);     // and return type
             }
             else if (tyarithmetic(ty) || tynullptr(ty))
-                ;
+            { }
             else if (ty == TYtemplate)
             {
                 // Not sure what to do with this, ignore.
@@ -161,9 +169,7 @@ void adl_scanType(list_t *pl, type *t)
             }
             else
             {
-#ifdef DEBUG
-                type_print(t);
-#endif
+                debug type_print(t);
                 //printf("ty = x%x\n", ty);
                 assert(0);
             }
@@ -178,31 +184,30 @@ void adl_scanType(list_t *pl, type *t)
 
 list_t adl_scanArglist(list_t arglist)
 {
-    list_t nclist = NULL;
+    list_t nclist = null;
 
-#if LOG
-    printf("adl_scanArglist()\n");
-#endif
+    debug (LOG) printf("adl_scanArglist()\n");
     for (list_t la = arglist; la; la = list_next(la))
     {
-        elem *e = (elem *)list_ptr(la);
+        elem *e = cast(elem *)list_ptr(la);
 
-#if LOG
-        elem_print(e);
-        type_print(e->ET);
-#endif
-        if (e->Eoper == OPrelconst && e->PEFflags & PEFtemplate_id)
+        debug (LOG)
         {
-            for (param_t *ptal = e->EV.sp.spu.Vtal; ptal; ptal = ptal->Pnext)
+            elem_print(e);
+            type_print(e.ET);
+        }
+        if (e.Eoper == OPrelconst && e.PEFflags & PEFtemplate_id)
+        {
+            for (param_t *ptal = e.EV.Vtal; ptal; ptal = ptal.Pnext)
             {
-                if (ptal->Ptype)
-                    adl_scanType(&nclist, ptal->Ptype);
-                else if (ptal->Pelem)
-                    adl_scanType(&nclist, ptal->Pelem->ET);
+                if (ptal.Ptype)
+                    adl_scanType(&nclist, ptal.Ptype);
+                else if (ptal.Pelem)
+                    adl_scanType(&nclist, ptal.Pelem.ET);
             }
         }
         else
-            adl_scanType(&nclist, e->ET);
+            adl_scanType(&nclist, e.ET);
     }
     return nclist;
 }
@@ -210,34 +215,30 @@ list_t adl_scanArglist(list_t arglist)
 /******************************************
  */
 
-symbol *adl_searchNamespace(const char *id, symbol *sn)
+Symbol *adl_searchNamespace(const(char)* id, Symbol *sn)
 {
-#if LOG
-    printf("adl_searchNamespace(id='%s', sn = '%s')\n", id, sn->Sident);
-#endif
-    return findsy(id, sn->Snameroot);
+    debug (LOG) printf("adl_searchNamespace(id='%s', sn = '%s')\n", id, sn.Sident);
+    return findsy(id, sn.Snameroot);
 }
 
 /******************************************
  */
 
-symbol *adl_searchClass(const char *id, Classsym *stag)
+Symbol *adl_searchClass(const(char)* id, Classsym *stag)
 {
-    return findsy(id, stag->Sstruct->Sroot);
+    return findsy(id, stag.Sstruct.Sroot);
 }
 
 /********************************************
  * Return list of function symbols via ADL.
  */
 
-symbol *adl_lookup(char *id, symbol *so, list_t arglist)
+Symbol *adl_lookup(char *id, Symbol *so, list_t arglist)
 {
     list_t nclist;
-    list_t symlist = NULL;
+    list_t symlist = null;
 
-#if LOG
-    printf("adl_lookup(id='%s')\n", id);
-#endif
+    debug (LOG) printf("adl_lookup(id='%s')\n", id);
     nclist = adl_scanArglist(arglist);
 
     if (!nclist)
@@ -245,42 +246,38 @@ symbol *adl_lookup(char *id, symbol *so, list_t arglist)
 
     for (list_t ncl = nclist; ncl; ncl = list_next(ncl))
     {
-        symbol *sa = (symbol *)list_ptr(ncl);
-        symbol *s;
+        Symbol *sa = cast(Symbol *)list_ptr(ncl);
+        Symbol *s;
 
-#if LOG
-        printf("\tlooking at associated namespace/class '%s'\n", sa ? sa->Sident : "");
+        debug (LOG) printf("\tlooking at associated namespace/class '%s'\n", sa ? sa.Sident : "");
         //symbol_print(sa);
-#endif
 
         if (!sa)
         {
             // Search global namespace
             s = scope_search(id, SCTglobal);
         }
-        else if (sa->Sclass == SCnamespace)
+        else if (sa.Sclass == SCnamespace)
         {
             s = adl_searchNamespace(id, sa);
         }
-        else if (sa->Sclass == SCstruct)
+        else if (sa.Sclass == SCstruct)
         {
-            s = adl_searchClass(id, (Classsym *)sa);
+            s = adl_searchClass(id, cast(Classsym *)sa);
         }
         else
         {
             assert(0);
         }
 
-#if LOG
         if (s)
-            printf("\tfound symbol '%s' in scope '%s'\n", s->Sident, s->Sscope ? s->Sscope->Sident : "");
-#endif
-        if (s && tyfunc(s->Stype->Tty) && s->Sclass != SCtypedef &&
-            s->Sclass != SCalias && s->Sclass != SCfuncalias)
         {
-#if LOG
-            printf("\tadding symbol '%s'\n", s->Sident);
-#endif
+            debug (LOG) printf("\tfound symbol '%s' in scope '%s'\n", s.Sident, s.Sscope ? s.Sscope.Sident : "");
+        }
+        if (s && tyfunc(s.Stype.Tty) && s.Sclass != SCtypedef &&
+            s.Sclass != SCalias && s.Sclass != SCfuncalias)
+        {
+            debug (LOG) printf("\tadding symbol '%s'\n", s.Sident);
             if (!list_inlist(symlist, s))
                 list_prepend(&symlist, s);
         }
@@ -294,18 +291,16 @@ symbol *adl_lookup(char *id, symbol *so, list_t arglist)
     if (so && !list_inlist(symlist, so))
         list_prepend(&symlist, so);
 
-    so = (symbol *)list_ptr(symlist);
+    so = cast(Symbol *)list_ptr(symlist);
     if (list_nitems(symlist) == 1)
     {
         list_free(&symlist, FPNULL);
         return so;
     }
 
-    symbol *sadl;
-
-    sadl = symbol_name(so->Sident, SCadl, so->Stype);
-    sadl->Spath = symlist;
+    Symbol* sadl = symbol_name(&so.Sident[0], SCadl, so.Stype);
+    sadl.Spath = symlist;
     return sadl;
 }
 
-#endif
+}
