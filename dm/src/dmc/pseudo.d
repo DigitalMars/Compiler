bc75429c2e37d4015a6f0c879c7d224f86e4ff35
Walter Bright <walter@walterbright.com>
2017-07-20 14:08:34 -0700

    switch to using X Macro

--- dm/src/dmc/pseudo.d
+++ dm/src/dmc/pseudo.d
@@ -32,149 +32,68 @@ import scopeh;
 
 extern (C++):
 
-/* 4 parallel tables
+/* 4 parallel tables using "X Macro" technique
+ */
 
-        X("AH",4,mAX,TYuchar)
-        X("AL",0,mAX,TYuchar)
-        X("AX",8,mAX,TYushort)
-        X("BH",7,mBX,TYuchar)
-        X("BL",3,mBX,TYuchar)
-        X("BP",13,0,TYushort)
-        X("BX",11,mBX,TYushort)
-        X("CH",5,mCX,TYuchar)
-        X("CL",1,mCX,TYuchar)
-        X("CX",9,mCX,TYushort)
-        X("DH",6,mDX,TYuchar)
-        X("DI",15,mDI,TYushort)
-        X("DL",2,mDX,TYuchar)
-        X("DX",10,mDX,TYushort)
-        X("EAX",16,mAX,TYulong)
-        X("EBP",21,0,TYulong)
-        X("EBX",19,mBX,TYulong)
-        X("ECX",17,mCX,TYulong)
-        X("EDI",23,mDI,TYulong)
-        X("EDX",18,mDX,TYulong)
-        X("ESI",22,mSI,TYulong)
-        X("ESP",20,0,TYulong)
-        X("SI",14,mSI,TYushort)
-        X("SP",12,0,TYushort)
-*/
+template Y(alias X)
+{
+    enum Y =
+    [
+        //  id   reg  mask   ty
+        X!("AH",   4, mAX, TYuchar),
+        X!("AL",   0, mAX, TYuchar),
+        X!("AX",   8, mAX, TYushort),
+        X!("BH",   7, mBX, TYuchar),
+        X!("BL",   3, mBX, TYuchar),
+        X!("BP",  13,   0, TYushort),
+        X!("BX",  11, mBX, TYushort),
+        X!("CH",   5, mCX, TYuchar),
+        X!("CL",   1, mCX, TYuchar),
+        X!("CX",   9, mCX, TYushort),
+        X!("DH",   6, mDX, TYuchar),
+        X!("DI",  15, mDI, TYushort),
+        X!("DL",   2, mDX, TYuchar),
+        X!("DX",  10, mDX, TYushort),
+        X!("EAX", 16, mAX, TYulong),
+        X!("EBP", 21,   0, TYulong),
+        X!("EBX", 19, mBX, TYulong),
+        X!("ECX", 17, mCX, TYulong),
+        X!("EDI", 23, mDI, TYulong),
+        X!("EDX", 18, mDX, TYulong),
+        X!("ESI", 22, mSI, TYulong),
+        X!("ESP", 20,   0, TYulong),
+        X!("SI",  14, mSI, TYushort),
+        X!("SP",  12,   0, TYushort),
+    ];
+}
 
 // Table for identifiers
-private __gshared const(char)*[24] pseudotab =
-[
-    "AH",
-    "AL",
-    "AX",
-    "BH",
-    "BL",
-    "BP",
-    "BX",
-    "CH",
-    "CL",
-    "CX",
-    "DH",
-    "DI",
-    "DL",
-    "DX",
-    "EAX",
-    "EBP",
-    "EBX",
-    "ECX",
-    "EDI",
-    "EDX",
-    "ESI",
-    "ESP",
-    "SI",
-    "SP",
-];
+
+template Xtab(alias A, alias B, alias C, alias D) { enum Xtab = A; }
+
+private __gshared const(char)*[24] pseudotab = Y!Xtab;
+
 
 // Register number to use in addressing mode
-__gshared ubyte[24] pseudoreg =
-[
-    4,
-    0,
-    8,
-    7,
-    3,
-    13,
-    11,
-    5,
-    1,
-    9,
-    6,
-    15,
-    2,
-    10,
-    16,
-    21,
-    19,
-    17,
-    23,
-    18,
-    22,
-    20,
-    14,
-    12,
-];
+
+template Xreg(alias A, alias B, alias C, alias D) { enum Xreg = B; }
+
+__gshared ubyte[24] pseudoreg = Y!Xreg;
+
 
 // Mask to use for registers affected
-__gshared regm_t[24] pseudomask =
-[
-    mAX,
-    mAX,
-    mAX,
-    mBX,
-    mBX,
-    0,
-    mBX,
-    mCX,
-    mCX,
-    mCX,
-    mDX,
-    mDI,
-    mDX,
-    mDX,
-    mAX,
-    0,
-    mBX,
-    mCX,
-    mDI,
-    mDX,
-    mSI,
-    0,
-    mSI,
-    0,
-];
+
+template Xmask(alias A, alias B, alias C, alias D) { enum Xmask = C; }
+
+__gshared regm_t[24] pseudomask = Y!Xmask;
+
 
 // Table for type of pseudo register variable
-private __gshared const(tym_t)[24] pseudoty =
-[
-    mTYvolatile | TYuchar,
-    mTYvolatile | TYuchar,
-    mTYvolatile | TYushort,
-    mTYvolatile | TYuchar,
-    mTYvolatile | TYuchar,
-    mTYvolatile | TYushort,
-    mTYvolatile | TYushort,
-    mTYvolatile | TYuchar,
-    mTYvolatile | TYuchar,
-    mTYvolatile | TYushort,
-    mTYvolatile | TYuchar,
-    mTYvolatile | TYushort,
-    mTYvolatile | TYuchar,
-    mTYvolatile | TYushort,
-    mTYvolatile | TYulong,
-    mTYvolatile | TYulong,
-    mTYvolatile | TYulong,
-    mTYvolatile | TYulong,
-    mTYvolatile | TYulong,
-    mTYvolatile | TYulong,
-    mTYvolatile | TYulong,
-    mTYvolatile | TYulong,
-    mTYvolatile | TYushort,
-    mTYvolatile | TYushort,
-];
+
+template Xty(alias A, alias B, alias C, alias D) { enum Xty = mTYvolatile | D; }
+
+private __gshared const(tym_t)[24] pseudoty = Y!Xty;
+
 
 //////////////////////////////////////
 // Given an undefined symbol s, see if it is in fact a pseudo
