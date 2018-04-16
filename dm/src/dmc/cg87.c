0ba8fd3d9588267f8766d11cc826c978df0a9831
Walter Bright <walter@walterbright.com>
2018-04-16 15:42:57 -0700

    cg87.c: remove shadowing of 'regregs'

--- dm/src/dmc/cg87.c
+++ dm/src/dmc/cg87.c
@@ -1510,8 +1510,8 @@ void orth87(CodeBuilder& cdb,elem *e,regm_t *pretregs)
         reverse ^= 1;
     }
 
-    regm_t retregs = mST0;
-    codelem(cdb,e1,&retregs,FALSE);
+    regm_t retregs1 = mST0;
+    codelem(cdb,e1,&retregs1,FALSE);
     note87(e1,0,0);
 
     if (config.flags4 & CFG4fdivcall && e->Eoper == OPdiv)
